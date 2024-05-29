f856da3723dcf5a00d4525a2e6f1751f06789f68
Tabish221 <143192083+Tabish221@users.noreply.github.com>
2024-05-29 18:35:45 +0500

    work check

---
+++ includes/header.php
@@ -0,0 +1,64 @@
+<header>
+	<div class="container">
+		<div class="main-header">
+			<div class="row align-items-center">
+				<div class="col-md-3 text-left">
+					<a href="./" class="logo">
+						<img src="assets/images/logo.png" alt="">
+					</a>
+				</div>
+				<div class="col-md-9">
+					<div class="head-right">
+						<div class="head-info">
+							<a href="#">
+								<div class="icon"><i class="fas fa-envelope-open-text"></i></div>
+								Info@Brandname.com
+							</a>
+	
+							<a href="#">
+								<div class="icon"><i class="fas fa-phone"></i></div>
+								Call Us: +44 (800) 0418230
+							</a>
+						</div>
+						<div class="head-chat">
+							<a href="#">
+								<div class="icon">
+									<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 60 60" style="enable-background:new 0 0 60 60;" xml:space="preserve">
+										<style type="text/css">
+											.st0 {
+												fill: #CECECE;
+											}
+		
+											.st1 {
+												fill: #FFFFFF;
+											}
+										</style>
+										<polygon class="st0" points="47.2,41.4 47.1,33.6 50.5,33 52.3,30.7 52.4,12.6 51.6,10.8 49.9,9.5 45.1,9.4 21.6,9.6 19.2,10.8   18.2,13.7 18.4,18.8 34.5,23.9 40.2,34.6 " />
+										<path class="st1" d="M38.4,41.6h-14l-7.7,8.9l-0.4-8.9h-5.7c-1.7,0-3.2-1.4-3.2-3.2V21.2c0-1.7,1.4-3.2,3.2-3.2h27.8  c1.7,0,3.2,1.4,3.2,3.2v17.2C41.6,40.2,40.2,41.6,38.4,41.6z" />
+										<path d="M14.5,26.2c-0.2,0-0.4-0.2-0.4-0.4s0.2-0.4,0.4-0.4h10.1c0.2,0,0.4,0.2,0.4,0.4s-0.2,0.4-0.4,0.4H14.5z" />
+										<path d="M14.5,30.8c-0.2,0-0.4-0.2-0.4-0.4s0.2-0.4,0.4-0.4h20.1c0.2,0,0.4,0.2,0.4,0.4s-0.2,0.4-0.4,0.4H14.5z" />
+										<path d="M14.5,35.5c-0.2,0-0.4-0.2-0.4-0.4s0.2-0.4,0.4-0.4h20.1c0.2,0,0.4,0.2,0.4,0.4s-0.2,0.4-0.4,0.4H14.5z" />
+										<path d="M16.8,51c-0.1,0-0.1,0-0.2,0c-0.2-0.1-0.3-0.2-0.3-0.4v-8.1h-5c-2.4,0-4.3-1.9-4.3-4.3V21.8c0-2.4,1.9-4.3,4.3-4.3h6.6v-4.3  c0-2.4,1.9-4.3,4.3-4.3L48.6,9c2.4,0,4.3,1.9,4.3,4.3v16.4c0,2.4-1.9,4.3-4.3,4.3h-1.2v8c0,0.2-0.1,0.4-0.3,0.4c-0.1,0-0.1,0-0.2,0  c-0.1,0-0.2-0.1-0.3-0.1L42,37.2v0.9c0,2.4-1.9,4.3-4.3,4.3l-13,0.1l-7.6,8.4C17.1,50.9,17,51,16.8,51z M11.4,18.5  C9.5,18.5,8,20,8,21.8v16.4c0,1.9,1.5,3.4,3.4,3.4h5.5c0.2,0,0.4,0.2,0.4,0.4v7.4l7-7.7c0.1-0.1,0.2-0.1,0.3-0.1l13.2-0.1  c1.9,0,3.4-1.5,3.4-3.4V21.8c0-1.9-1.5-3.4-3.4-3.4H18.4L11.4,18.5z M37.8,17.5c2.4,0,4.3,1.9,4.3,4.3v14.1l4.5,5v-7.4  c0-0.2,0.2-0.4,0.4-0.4h1.6c1.7,0,3.2-1.3,3.4-3l0,0v-0.3V13.3c0-1.9-1.5-3.4-3.4-3.4H22.2c-1.9,0-3.4,1.5-3.4,3.4v4.3L37.8,17.5  L37.8,17.5z" />
+									</svg>
+								</div>
+		
+								<div class="cont">
+									<span>24/7 Available</span>
+									<b>Live Chat</b>
+								</div>
+							</a>
+						</div>
+						<div class="head-btn">
+							<a href="#">
+								<span>
+									<div class="icon"><i class="fas fa-edit"></i></div>
+									GET IN TOUCH
+								</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <?php include("includes/compatibility.php"); ?>
+      <meta name="description" content="">
+      <title>Title Here</title>
+      <?php include("includes/style.php"); ?>
+   </head>
+   <body>
+      <?php include("includes/header.php"); ?>
+
+      <section class="mainBanner" style="background-image: url(assets/images/banner/bg-banner.jpg);">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="banner-timer">
+                     <img src="assets/images/banner/timer.png" alt="Timer">
+                  </div>
+
+                  <div class="banner-cont">
+                     <div class="banner-priceTag">
+                        <img src="assets/images/banner/price-tag.png" alt="Price Tag">
+                     </div>
+
+                     <h2>GET YOUR CUSTOM</h2>
+                     <h1>Logo Design</h1>
+                     <p>Your Business deserves an identity as unique as itself. Give us the opportunity to help you rise above the competition with eye-catching and memorable Custom Logo Designs.</p>
+                     <ul>
+                        <li>1 Illustration Design</li>
+                        <li>Money Back Guarantee</li>
+                        <li>1 Revision</li>
+                        <li>Turn Around Time 4 Days</li>
+                     </ul>
+
+                     <a href="#">ACTIVATE YOUR COUPON <span class="icon"><i class="fas fa-arrow-circle-right"></i></span></a>
+                  </div>
+
+                  <div class="banner-rating">
+                     <img src="assets/images/banner/rating.png" alt="Rating">
+                  </div>
+               </div>
+
+               <div class="col-md-6">
+                  <div class="banner-img">
+                     <img src="assets/images/banner/img.png" alt="Images">
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+
+      <section class="section1">
+         <div class="container">
+            <div class="row align-items-center">
+               <div class="col-md-3">
+                  <div class="sec1-cont">
+                     <span>More Than</span>
+                     <b>200,000+</b>
+                     <i>Client For Our <br> Logo Design Projects</i>
+                  </div>
+               </div>
+
+               <div class="col-md-9">
+                  <div class="row">
+                     <div class="col">
+                        <div class="sec1-card">
+                           <div class="icon">
+                              <img src="assets/images/sec1/icon1.png" alt="ICON">
+                           </div>
+                           <p>100% Money-back Gurantee</p>
+                        </div>
+                     </div>
+
+                     <div class="col">
+                        <div class="sec1-card">
+                           <div class="icon">
+                              <img src="assets/images/sec1/icon2.png" alt="ICON">
+                           </div>
+                           <p>100% Sarisfaction Gurantee</p>
+                        </div>
+                     </div>
+
+                     <div class="col">
+                        <div class="sec1-card">
+                           <div class="icon">
+                              <img src="assets/images/sec1/icon3.png" alt="ICON">
+                           </div>
+                           <p>100% Custom Design</p>
+                        </div>
+                     </div>
+
+                     <div class="col">
+                        <div class="sec1-card">
+                           <div class="icon">
+                              <img src="assets/images/sec1/icon4.png" alt="ICON">
+                           </div>
+                           <p>Unlimited <br> Revision</p>
+                        </div>
+                     </div>
+
+                     <div class="col">
+                        <div class="sec1-card">
+                           <div class="icon">
+                              <img src="assets/images/sec1/icon5.png" alt="ICON">
+                           </div>
+                           <p>24/7 Customer Support</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+
+      <section class="section2">
+         <div class="container">
+            <div class="sec2-top">
+               <div class="row align-items-center">
+                  <div class="col-md-6">
+                     <div class="sec2-hd mn-hd">
+                        <h5>Logo Designs</h5>
+                        <h6><span>Types For Every Venture</span></h6>
+                     </div>
+                  </div>
+
+                  <div class="col-md-6">
+                     <div class="sec2-hd2 mn-hd">
+                        <p class="p1">Need Something Special For Your Brand?We Have Got You Covered!</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+            <div class="sec2-bottom">
+               <div class="row align-items-stretch">
+                  <div class="col">
+                     <div class="sec2-card">
+                        <div class="icon">
+                           <img src="assets/images/sec2/icon1.png" alt="Icon">
+                        </div>
+
+                        <div class="cont">
+                           <h6>Iconic <br> Logo Design</h6>
+
+                           <p>As an all-rounder video animation company, we are focused on providing state-of-the-art videos with stunning custom-made visuals</p>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div class="col">
+                     <div class="sec2-card">
+                        <div class="icon">
+                           <img src="assets/images/sec2/icon2.png" alt="Icon">
+                        </div>
+
+                        <div class="cont">
+                           <h6>Typographiv <br> Logo Design</h6>
+
+                           <p>As an all-rounder video animation company, we are focused on providing state-of-the-art videos with stunning custom-made visuals</p>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div class="col">
+                     <div class="sec2-card">
+                        <div class="icon">
+                           <img src="assets/images/sec2/icon3.png" alt="Icon">
+                        </div>
+
+                        <div class="cont">
+                           <h6>Illustration <br> Logo Design</h6>
+
+                           <p>As an all-rounder video animation company, we are focused on providing state-of-the-art videos with stunning custom-made visuals</p>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div class="col">
+                     <div class="sec2-card">
+                        <div class="icon">
+                           <img src="assets/images/sec2/icon4.png" alt="Icon">
+                        </div>
+
+                        <div class="cont">
+                           <h6>Animated <br> Logo Design</h6>
+
+                           <p>As an all-rounder video animation company, we are focused on providing state-of-the-art videos with stunning custom-made visuals</p>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div class="col">
+                     <div class="sec2-card last">
+                        <div class="icon">
+                           <img src="assets/images/sec2/icon5.png" alt="Icon">
+                        </div>
+
+                        <div class="cont">
+                           <h6>Static 3D <br> Logo Design</h6>
+
+                           <p>As an all-rounder video animation company, we are focused on providing state-of-the-art videos with stunning custom-made visuals</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+
+      <section class="cta cta1">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-4">
+                  <div class="cta-hd">
+                     <h6>Limited</h6>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+
+
+      <?php include("includes/footer.php"); ?>
+      <?php include("includes/scripts.php"); ?>
+   </body>
+</html>
